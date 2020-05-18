65ea54d2fce6805a6acecc8218e543ec82219dfc
Nguyễn Uyên <55393354+NguyenUyen0904@users.noreply.github.com>
2020-05-18 12:38:54 +0700

    Add files via upload

--- KTMT9.asm
+++ KTMT9.asm
@@ -15,88 +15,87 @@ Main:
 	lw $s1,MAX_OF_SV #max of sinh vien  doc d? lieu word tu bo nho vao thanh ghi s1
 	#nhap n( Nhap so luong sinh vien )
 	li $v0,51# goi hop thoai nhap vao so luong sinh  
-	la $a0,Number_Of_SV #$a0 tiêu ??: Nh?p vào s? l??ng sinh viên MAX=10
-	syscall#$a0 nh?p vào s? sinh viên 
+	la $a0,Number_Of_SV #$a0 tieu de: nhap vao oso luong sinh vien Max=10
+	syscall#$a0 Nhap vao so luong  sinh vien 
 		
-	slt $t0,$s1,$a0  # so sánh $s1 < $a0 ? n?u ?úng thì $t0=1( so sánh s? sinh viên max có bé h?n s? sinh viên nh?p vào ko ??)
-	bne $t0,$0,Main #n?u ($t0!=0) thì nh?y l?i l?nh main ( n?u có bé h?n thì l?p l?i hàm main)
-	slt $t0,$0,$a0# còn n?u ($t0#0) sai hay $0<$a0 ?úng thì th?c hi?n so sánh ti?p $0<$a0??( so sánh ti?p s? sinh viên nh?p vào có l?n h?n 0 ko ??)
-	bne $t0,$0,Continue #n?u ($t0!=$0) t?c là ($0<$a0) ?úng thì nh?y l?nh Countinue( N?u mà s? sinh viên l?n h?n 0 thì nh?y sang l?nh Continue)
-	li $v0,55# n?u 0<$a0 g?i h?p tho?i thông báo ( còn n?u ko thì g?i h?p tho?i thông báo r?ng )
-	la $a0,	Message1# $a0 tiêu ?? :"so luong SV phai > 0"
-	syscall# Nh?p l?i $a0 la s? l??ng sinh viên c?a l?p 
+	slt $t0,$s1,$a0  # so sánh $s1 < $a0 ? neu dung thi $t0=1( so sanh so sinh vien max < so sinh vien nhap vao ko ??)
+	bne $t0,$0,Main #neu($t0!=0) thì nhay lai lenh main (neu < thi lap lai ham main)
+	slt $t0,$0,$a0# con neu ($t0!=0) sai hay $0<$a0 dunng thì thuc hien so sanh tiep $0<$a0??( so sanh so sinh vien nhap vao>0 ko ??)
+	bne $t0,$0,Continue #neu ($t0!=$0)hay ($0<$a0) dung thi nhay lenh Countinue( neu so sinh vien nhap vao >0 thi nhay sang lenh Continue)
+	li $v0,55# neu 0<$a0 goi hop thong bao
+	la $a0,	Message1# $a0 tieu de :"so luong SV phai > 0"
+	syscall
 	j Main
 Continue:
 	#khoi tao gia tri các giá tr?
-	add $s0,$a0,$0 # $gán s0 = n =a0= so luong sv
-	la $s1,SV #??a ch? c? s? m?ng SV
-	add $t1,$0,$s0  # i = n
-	addi $s3,$s1,0  #s3=sv[0]
+	add $s0,$a0,$0 # $ gan s0 = n( =a0= so luong sv)
+	la $s1,SV # nap dia chi cua mang s1
+	add $t1,$0,$s0  # Khoi tao i = n
+	addi $s3,$s1,0  #Khoi tao s3=sv[0]
 NhapMang:
-	beq $t1,0,Done  #n?u t1==0 thi nh?y sang Done 
-	li $v0,54 # g?i h?p tho?i nh?p tên sinh viên  
-	la $a0,Name_Of_SV# tiêu ?? Name_Of_SV
-	la $a1,0($s3)# ch? ra v? trí L?u tên 
-	la $a2,16# gi?i h?n ?? dài là 16 kí t? 
-	syscall# nh?p tên 
-
-	addi $s3,$s3,16	
+	beq $t1,0,Done  #neu t1==0 thi nhay sang Done 
+	li $v0,54 # con neu ko thi goi hop thoai nhap ten sinh vien   
+	la $a0,Name_Of_SV# tieu de Name_Of_SV
+	la $a1,0($s3)# chi ra vi tri luu ten 
+	la $a2,16# gioi han do dai laf 16 ki tu  
+	syscall# nhap ten
+	addi $s3,$s3,16	# chuyen sang vi tri tiep theo 
 Mark:
 	#nhap diem
-	li $v0,51# g?i h?p tho?i
-	la $a0,Diem# tiêu ?? : Diem
-	syscall# Nh?p ?i?m 
-	slti $t0,$a0,11# so sánh a0<11 ?úng thì t0=1
-	beq $t0,$0,Mark# n?u t0==0 thì nh?y l?i l?nh Mark
+	li $v0,51# goi hop thoai
+	la $a0,Diem# tieu de : Diem
+	syscall# Nhap diem
+	slti $t0,$a0,11# so sánh diem nhap vao a0<11 dung thi t0=1
+	beq $t0,$0,Mark# neu t0==0 thì nhay lai lenh Mark( nghia la khi nay a0>10)
 	
-	slt $t0,$0,$a0# còn n?u không so sánh 0<a0,n?u ?úng t0=1
-	bne $t0,$0,Continue1#n?u t0=0 thì nh?y sang l?nh Continue1
-	li $v0,55 # n?u không thì g?i h?p tho?i thông báo 
-	la $a0,	Check_Mark# tiêu dè Check_mark
-	syscall# nh?n ok
-	j Mark #Nh?y l?i l?nh mark
+	slt $t0,$0,$a0# con neu ko thi so sánh 0<a0,neu dung t0=1
+	bne $t0,$0,Continue1#neu t0=0 thì nhay sang lenh Continue1
+	li $v0,55 # neu ko thi goi hop thong bao
+	la $a0,	Check_Mark# tieu de:"diem >0 and diem <10"
+	syscall# nhan ok
+	j Mark #va nhay lai lenh main
 Continue1:	
-	sw $a0,0($s3)# c?t ??a ch? c? s? s3 vào thanh ghi b? nh? a0
-	addi $s3, $s3, 4# nh?y ??n ph?n t? m?ng ti?p theo 
-	addi $t1,$t1,-1# kh?i t?o l?i giá tr? t1=0
+	sw $a0,0($s3)# cat dia chi cua s3 vao thanh ghi a0
+	addi $s3, $s3, 4# nhay sang phan tu tiep theo cua s3
+	addi $t1,$t1,-1# Khoi tao lai t1=t1-1
 	
-	j NhapMang# Nh?y v? l?nh nh?p m?ng 
+	j NhapMang# nhay lai lenh NhapMang
 Done:
-	li $v0,4# g?i k?t qu? 
-	la $a0,Message	# tiêu ?? "Danh sach sinh vien truot la \n"
+	li $v0,4# thong bao ket qua 
+	la $a0,Message	# tieu de: "Danh sach sinh vien truot la \n"
 	syscall
 Print_name:
 	addi $t3, $0,0 # i = 0 
-	add $s4,$0,$s1 # ??a ch? sv[0]
+	add $s4,$0,$s1 # dia chi sv[0]
 	
 for:	add $s5,$s4, $0 #s5=s4
 
 	addi $t5,$s4,16 # sv[0]+16
-	lw   $t6,0($t5) # n?p  sv[0].diem
+	lw   $t6,0($t5) #nap sv[0].diem
 	
-	slti $t7,$t6,5# ?i?m check là 5, so sánh ?i?m nh?p vào v?i 5, n?u bé h?n thì t7=1
-	bne $t7,$0,print_char # n?u t7!=0 thì nh?y sang l?nh print_char
+	slti $t7,$t6,5# ?diem check là 5, so sánh diem nhap vao voi 5, neu < thì t7=1
+	bne $t7,$0,print_char # n?u t7!=0(hay t7=1) thì nhay sang lenh print_char
 	j next# còn n?u không thì nh?y sang l?nh next
 
 print_char: #in ten
 	lb $t2,0($s5) # load byte
-	beq $t2,10, end_for # = \n. n?u t2==10 thì nh?y l?nh end_for
-	addi $a0,$t2,0 # n?u không thì a0 = t2
+	beq $t2,10, end_for # = \n. neu t2==10 thì nhay lenh end_for
+	addi $a0,$t2,0 # neu khong thi a0 = t2
 	li $v0, 11 # print
 	syscall
 	addi $s5, $s5, 1 # j++
 	j print_char 
 end_for:
 	li $v0,11# in char
-	li $a0,10# thoát ch??ng trình 
+	li $a0,10# thoat chuong trinh
 	syscall
 	
 next:
 	addi $t3, $t3,1 #sv[i++]
-	beq $t3, $s0, end_print #i = n? thì nh?y sang end_print 
+	beq $t3, $s0, end_print #i = n? thi nhay sang lenh end_print 
 	mul $t4,$t3,20	#i*20
 	add $s4,$s1,$t4 # address sv[i] 
-	j for# Nh?y l?i l?nh for
+	j for# nhay sang lenh for
 
 end_print:
 	
